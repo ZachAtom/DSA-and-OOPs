d2783bb4f5bf62d2c83dda936d74f96be1c9241a
ZachAtom <jacksonokram91@gmail.com>
2025-04-17 07:55:53 +0530

    Learn difference between class variable and instance variable

--- employeeclass.py
+++ employeeclass.py
@@ -1,12 +1,22 @@
 class Employee:
-    def __init__(self, first, last, address):
+    Num_of_emps = 0
+    raise_amount = 1.04
+    def __init__(self, first, last, address, pay):
         self.first = first
         self.last = last
         self.address = address
+        self.pay = pay
+        Employee.Num_of_emps += 1
     def full_name(self):
         return f"{self.first} {self.last}"
-        
-
-emp_1 = Employee("Zach", "Atom", "Thongju")
-emp_2 = Employee("John", "Doe", "Kangpokpi")
-print(emp_1.full_name())    
+    def apply_raise(self):
+         self.pay = int(self.pay * self.raise_amount)
+            
+print(Employee.Num_of_emps)
+emp_1 = Employee("Zach", "Atom", "Thongju", 50000)
+emp_2 = Employee("John", "Doe", "Kangpokpi", 50000)
+emp_1.raise_amount = 1.05
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_1.pay)
+print(Employee.Num_of_emps)
