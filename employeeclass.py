254a78789ded0a59ad4e6874cbacfc86f59bf849
ZachAtom <jacksonokram91@gmail.com>
2025-04-18 23:11:45 +0530

    Learn classmethod and staticmethod

--- employeeclass.py
+++ employeeclass.py
@@ -11,12 +11,21 @@ class Employee:
         return f"{self.first} {self.last}"
     def apply_raise(self):
          self.pay = int(self.pay * self.raise_amount)
-            
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+import datetime
+my_date = datetime.date(2023, 10, 9)
+print(Employee.is_workday(my_date))        
 print(Employee.Num_of_emps)
 emp_1 = Employee("Zach", "Atom", "Thongju", 50000)
 emp_2 = Employee("John", "Doe", "Kangpokpi", 50000)
-emp_1.raise_amount = 1.05
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
-print(Employee.Num_of_emps)
+Employee.set_raise_amt(1.05)
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
