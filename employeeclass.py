d2f2576bb0c353ffe0481eccbbcfcbd4e730ae52
ZachAtom <jacksonokram91@gmail.com>
2025-04-19 21:53:11 +0530

    Learn subclass and inheritance

--- employeeclass.py
+++ employeeclass.py
@@ -1,6 +1,6 @@
 class Employee:
     Num_of_emps = 0
-    raise_amount = 1.04
+    raise_amount = 1.10
     def __init__(self, first, last, address, pay):
         self.first = first
         self.last = last
@@ -19,13 +19,35 @@ class Employee:
         if day.weekday() == 5 or day.weekday() == 6:
             return False
         return True
-import datetime
-my_date = datetime.date(2023, 10, 9)
-print(Employee.is_workday(my_date))        
-print(Employee.Num_of_emps)
-emp_1 = Employee("Zach", "Atom", "Thongju", 50000)
-emp_2 = Employee("John", "Doe", "Kangpokpi", 50000)
-Employee.set_raise_amt(1.05)
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+class Developer(Employee):
+    raise_amount = 1.20
+    def __init__(self, first, last, address, pay, prog_lang):
+        super().__init__(first, last, address, pay)
+        self.prog_lang =  prog_lang
+
+class Manager(Employee):
+    raise_amount = 1.15
+    def __init__(self, first, last, address, pay, employees=None):
+        super().__init__(first, last, address, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+    def print_emps(self):
+        for emp in self.employees:
+            print("-->", emp.full_name())
+dev_1 = Developer("Zach", "Atom", "Thongju", 50000, "Python")
+dev_2 = Developer("John", "Doe", "Kangpokpi", 50000, "Java")
+mgr_1 = Manager("Luke", "Skywalker", "Delhi", 60000, [dev_1])
+mgr_1.add_emp(dev_2)
+mgr_1.print_emps()
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
+print(issubclass(Developer, Employee))
+print(issubclass(Manager, Employee))
