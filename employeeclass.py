d7a51ce5ed8e880624e2e44328597a5b505cfdcc
ZachAtom <jacksonokram91@gmail.com>
2025-04-20 22:43:41 +0530

    Learn Magic/Dunder Method

--- employeeclass.py
+++ employeeclass.py
@@ -6,17 +6,34 @@ class Employee:
         self.last = last
         self.address = address
         self.pay = pay
+        # Increment the number of employees when a new instance is created
         Employee.Num_of_emps += 1
+    @property
+    def email(self):
+        return f"{self.first}.{self.last}@company.com"
+    @property
     def full_name(self):
         return f"{self.first} {self.last}"
+    @full_name.setter
+    def full_name(self, name):
+        first, last = name.split()
+        self.first = first
+        self.last = last
+    @full_name.deleter
+    def full_name(self):
+        print("Deleting Name!")
+        del self.first
+        del self.last
     def apply_raise(self):
          self.pay = int(self.pay * self.raise_amount)
-            
-print(Employee.Num_of_emps)
+    def __repr__(self):
+        return f"Employee({self.first}, {self.last}, {self.address}, {self.pay})"
+    def __str__(self):
+        return f"Employee: {self.first} {self.last}, Address: {self.address}, Pay: {self.pay}"
 emp_1 = Employee("Zach", "Atom", "Thongju", 50000)
 emp_2 = Employee("John", "Doe", "Kangpokpi", 50000)
-emp_1.raise_amount = 1.05
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
-print(Employee.Num_of_emps)
+emp_1.full_name = "Lord Zach"
+print(emp_1.email)
+print(emp_1.first)
+print(repr(emp_1))
+print(str(emp_1))
